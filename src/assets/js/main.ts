1fddad72d89ba171b4ecb9f78e53d26163a9e527
Phat Lam <phat.lam@emurgovn.io>
2018-05-30 14:42:33 +0700

    Add dummy page

---
+++ src/assets/js/main.ts
@@ -0,0 +1,42 @@
+$(document).ready( () => {
+
+    const navListItems = $("div.setup-panel div a"),
+          allWells = $(".setup-content"),
+          allNextBtn = $(".nextBtn");
+
+    allWells.hide();
+
+    navListItems.click((e) => {
+        e.preventDefault();
+        const $target = $($(this).attr("href")),
+            $item = $(this);
+
+        if (!$item.hasClass("disabled")) {
+            navListItems.removeClass("btn-success").addClass("btn-default");
+            $item.addClass("btn-success");
+            allWells.hide();
+            $target.show();
+            $target.find("input:eq(0)").focus();
+        }
+    });
+
+    allNextBtn.click(function () {
+        const curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $("div.setup-panel div a[href=\"#" + curStepBtn + "\"]").parent().next().children("a"),
+            curInputs: any = curStep.find("input[type=\"text\"],input[type=\"url\"]");
+        let isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for (let i = 0; i < curInputs.length; i++) {
+            if (!curInputs[i].validity.valid) {
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+
+        if (isValid) nextStepWizard.removeAttr("disabled").trigger("click");
+    });
+
+    $("div.setup-panel div a.btn-success").trigger("click");
+});
--- src/routes/app.ts
+++ src/routes/app.ts
@@ -3,6 +3,7 @@ import { Router } from  "express";
 // Controllers (route handlers)
 import * as homeController from "../controllers/home";
 import * as userController from "../controllers/user";
+import * as assignmentController from "../controllers/assignment";
 const router = Router();
 
 // middleware that is specific to this router
@@ -20,5 +21,9 @@ router.get("/logout", userController.getLogout);
 router.post("/login", userController.postLogin);
 router.get("/sign-in", homeController.signIn);
 router.get("/sign-up", homeController.signUp);
+
+router.get("/blockchain101/assignment/transaction", assignmentController.transaction);
+router.get("/blockchain101/assignment/block", assignmentController.block);
+router.get("/blockchain101/assignment/quiz", assignmentController.quiz);
 
 module.exports = router;
