729a7f6cd3f7ee78ae6a6af8262e693a929807c9
Phat Lam <phat.lam@emurgovn.io>
2018-05-31 17:49:42 +0700

    Update style code

--- src/controllers/assignment.ts
+++ src/controllers/assignment.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from "express";
 
 export let transaction = (req: Request, res: Response) => {
-    res.render("assignment/transaction", {
-    });
+  res.render("assignment/transaction", {
+  });
 };
 
 export let block = (req: Request, res: Response) => {
-    res.render("assignment/block", {
-    });
+  res.render("assignment/block", {
+  });
 };
 
 export let quiz = (req: Request, res: Response) => {
-    res.render("assignment/quiz", {
-    });
+  res.render("assignment/quiz", {
+  });
 };
