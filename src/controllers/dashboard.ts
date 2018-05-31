a5051619ac7120dfb6c3d4b8b47d805f201b9e55
Phat Lam <phat.lam@emurgovn.io>
2018-05-31 14:53:15 +0700

    Demo dashboard page

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -6,3 +6,17 @@ export let index = (req: Request, res: Response) => {
     }
     res.render("dashboard/index");
 };
+
+export let profile = (req: Request, res: Response) => {
+    if (!req.user) {
+        return res.redirect("/sign-in");
+    }
+    res.render("dashboard/profile");
+};
+
+export let courses = (req: Request, res: Response) => {
+    if (!req.user) {
+        return res.redirect("/sign-in");
+    }
+    res.render("dashboard/courses");
+};
--- src/routes/app.ts
+++ src/routes/app.ts
@@ -22,6 +22,8 @@ router.get("/sign-up", userController.signUp);
 router.post("/sign-up", userController.postSignUp);
 
 router.get("/dashboard", dashboardController.index);
+router.get("/dashboard/profile", dashboardController.profile);
+router.get("/dashboard/courses", dashboardController.courses);
 
 router.get("/blockchain101/assignment/transaction", assignmentController.transaction);
 router.get("/blockchain101/assignment/block", assignmentController.block);
