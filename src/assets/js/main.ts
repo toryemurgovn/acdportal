8e34fd7d0f1ab7a2c38b00cc6c1490eeed4f67c7
Phat Lam <phat.lam@emurgovn.io>
2018-06-01 16:03:36 +0700

    Add dummy assignment page

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -101,3 +101,256 @@ const generateLicenseCode = () => {
     location.reload();
   });
 };
+
+$(document).ready(function () {
+  const jAssignment1 = $("#jsubmit1");
+  const jAssignment2 = $("#jsubmit2");
+  const jAssignment3 = $("#jsubmit3");
+  const jAssignment4 = $("#jsubmit4");
+  const jAssignment5 = $("#jsubmit5");
+  const jAssignment6 = $("#jsubmit6");
+  const jAssignment7 = $("#jsubmit7");
+  const jAssignment8 = $("#jsubmit8");
+  const jAssignment9 = $("#jsubmit9");
+
+  jAssignment1.on("click", (event) => {
+    event.preventDefault();
+    const data = {
+      name: $("input[name=trs1_name]").val(),
+      amount: $("input[name=trs1_amount]").val()
+    };
+
+    $.ajax({
+      url: "transaction/1",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jtrs1name").text(jdata[1]);
+        $(".jtrs1amount").text(jdata[3]);
+        $(".jtrs1hash").text(jdata[5]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  jAssignment2.on("click", (event) => {
+    event.preventDefault();
+    const data = {
+      passphrase: $("input[name=trs2Passphrase]").val()
+    };
+
+    $.ajax({
+      url: "transaction/2",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jtrs2prikey").text(jdata[1]);
+        $(".jtrs2pubkey").text(jdata[3]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  $(".jcopytrs2prikey").on("click", (event) => {
+    $("#trs3prkey").val($(".jtrs2prikey").text());
+  });
+
+  jAssignment3.on("click", (event) => {
+    event.preventDefault();
+    const data = {
+      name: $("input[name=trs3name]").val(),
+      amount: $("input[name=trs3amount]").val(),
+      prikey: $("input[name=trs3prkey]").val()
+    };
+
+    $.ajax({
+      url: "transaction/3",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jtrs3name").text(jdata[1]);
+        $(".jtrs3amount").text(jdata[3]);
+        $(".jtrs3hash").text(jdata[5]);
+        $(".jtrs3signature").text(jdata[9]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  $(".jcopytrs3sign").on("click", (event) => {
+    $("#trs4sign").val($(".jtrs3signature").text());
+  });
+
+  jAssignment4.on("click", (event) => {
+    event.preventDefault();
+    const data = {
+      name: $("input[name=trs4name]").val(),
+      amount: $("input[name=trs4amount]").val(),
+      pubkey: $("input[name=trs4pubkey]").val(),
+      sign: $("input[name=trs4sign]").val()
+    };
+
+    $.ajax({
+      url: "transaction/4",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jtrs4name").text(jdata[1]);
+        $(".jtrs4amount").text(jdata[3]);
+        $(".jtrs4hash").text(jdata[5]);
+        $(".jtrs4valid").text(jdata[11]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  jAssignment5.on("click", (event) => {
+    event.preventDefault();
+    $.ajax({
+      url: "transaction/5",
+      method: "POST",
+      data: {},
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jblock1info").text(jdata[1]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  jAssignment6.on("click", (event) => {
+    event.preventDefault();
+    $.ajax({
+      url: "transaction/6",
+      method: "POST",
+      data: {},
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jblock2info").text(jdata[1]);
+        $(".jblock2hash").text(jdata[2]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  jAssignment7.on("click", (event) => {
+    event.preventDefault();
+    const data = {
+      nonce: $("input[name=block3nonce]").val()
+    };
+
+    $.ajax({
+      url: "transaction/7",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jblock3info").text(jdata[1]);
+        $(".jblock3hash").text(jdata[2]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  jAssignment8.on("click", (event) => {
+    event.preventDefault();
+    const data = {
+      name: $("input[name=block4name]").val(),
+      amount: $("input[name=block4amount]").val(),
+      difficult: $("input[name=block4difficult]").val(),
+    };
+
+    $.ajax({
+      url: "transaction/8",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jblock4time").text(jdata[0]);
+        $(".jblock4info").text(jdata[1]);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+
+  jAssignment9.on("click", (event) => {
+    event.preventDefault();
+    const data = <any>{};
+    $(".input-data").each(function () {
+      data[$(this).attr("name")] = $(this).val();
+    });
+
+    $.ajax({
+      url: "transaction/9",
+      method: "POST",
+      data: data,
+      cache: false,
+      success: function (resData) {
+        const jdata = parseData(resData);
+        $(".jbock5data").text(resData);
+      },
+      error: function (xhr, status, err) {
+        showErrorMessage();
+        console.error(status, err.toString());
+      }
+    });
+    return false;
+  });
+
+  const parseData = (resData: any) => {
+    return resData.replace(/\r\n/g, "\r").replace(/\n/g, "\r").replace(/['"]+/g, "").split(/\r/).map(s => s.trim()).filter(Boolean)
+      ;
+  };
+
+  const showErrorMessage = () => {
+    (<any>$("#jerrormodal")).modal({
+      show: true,
+      keyboard: false
+    });
+  };
+});
--- src/controllers/assignment.ts
+++ src/controllers/assignment.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { quizzesData } from "../util/quizzes";
 import * as _ from "lodash";
+import * as childProcess from "child_process";
+
+const execFileSync = childProcess.execFileSync;
 
 export let transaction = (req: Request, res: Response) => {
   res.render("assignment/transaction", {
@@ -38,4 +41,89 @@ const checkQuestions = (data: any) => {
     }
   }
   return questions;
+};
+
+export let transactionMechanisms = (req: Request, res: Response) => {
+  const fnTest = "test" + req.params.id;
+  res.send(fn[fnTest](req.body));
+};
+
+const filePath = "./files";
+const fn = <any>{};
+
+fn.test1 = (data: any) => {
+  const inputData = data.name + "\n" + data.amount + "\n";
+  const file = filePath + "/test_1";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+
+fn.test2 = (data: any) => {
+  const inputData = data.passphrase + "\n";
+  const file = filePath + "/test_2";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test3 = (data: any) => {
+  const inputData = data.name + "\n" + data.amount + "\n" + data.prikey + "\n";
+  const file = filePath + "/test_3";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test4 = (data: any) => {
+  const inputData = data.name + "\n" + data.amount + "\n" + data.pubkey + "\n" + data.sign + "\n";
+  const file = filePath + "/test_4";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test5 = (data: any) => {
+  const inputData = "\n";
+  const file = filePath + "/test_5";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test6 = (data: any) => {
+  const inputData = "\n";
+  const file = filePath + "/test_6";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test7 = (data: any) => {
+  const inputData = data.nonce + "\n";
+  const file = filePath + "/test_7";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test8 = (data: any) => {
+  const inputData = data.name + "\n" + data.amount + "\n" + data.difficult + "\n";
+  const file = filePath + "/test_8";
+  return execFileSync(file, [], {
+    input: inputData
+  });
+};
+
+fn.test9 = (data: any) => {
+  let inputData = "";
+  for (const i in data) {
+    inputData += data[i] + "\n ";
+  }
+  inputData += "\n \n " + "\n \n " + "\n \n  " + "\n \n";
+  const file = filePath + "/test_9";
+  return execFileSync(file, [], {
+    input: inputData
+  });
 };
